e1d136108918c26289ceae4efbe0862c6d784c94
Likith Vadranapu <vadranapulikith@gmail.com>
2026-04-05 14:53:57 +0530

    fix api urls

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,7 +7,7 @@ function Login({ setUser }) {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post("https://chat-app-98qi.onrender.com/login", {
         email,
         password,
       });
