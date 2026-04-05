8df47f60d615af3b6e5815b0d5adbcc1f5611578
Likith Vadranapu <vadranapulikith@gmail.com>
2026-04-05 13:45:49 +0530

    fix db connection

--- server/db.js
+++ server/db.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
 
+console.log("DB HOST:", process.env.DB_HOST); // debug
+
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "chat_app"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("DB Error:", err);
-        return;
-    }
-    console.log("MySQL Connected ✅");
+  if (err) {
+    console.error("DB Error:", err);
+    return;
+  }
+  console.log("MySQL Connected ✅");
 });
 
 module.exports = db;
